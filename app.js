67d8020b57388e4823fafd22e4ca4c98e8baa1a2
sandesh-java-21 <85265264+sandesh-java-21@users.noreply.github.com>
2023-05-12 01:05:43 +0500

    changes in cors

--- app.js
+++ app.js
@@ -43,10 +43,7 @@ const io = require("socket.io")(http, {
 });
 
 app.use(function (req, res, next) {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://newzly-zabefest.000webhostapp.com"
-  ); // replace with your frontend URL
+  res.header("Access-Control-Allow-Origin", "*"); // replace with your frontend URL
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
